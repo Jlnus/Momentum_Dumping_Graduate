69d77efc278a8c59e34765c0ccb4cc64df1f9cc7
Jinus <zmdk0902@naver.com>
2026-03-27 10:47:38 +0900

    add TC dynamics

--- SC_TwoBody_Model_implement/model_init.m
+++ SC_TwoBody_Model_implement/model_init.m
@@ -14,18 +14,32 @@ dt = 0.01;
 %		(5) - argument of perigee in radians.
 %		(6) - mean anomaly in radians.
 SC_Kepler = [(6371.2+600)*1000, 0.00221, 0*pi/180, 0*pi/180, 0, deg2rad(270)];
-TC_Kepler = [(6371.2+600)*1000, 0.00221, 0*pi/180, 0*pi/180, 0, deg2rad(270)+0.000001];
+TC_Kepler = [(6371.2+600)*1000, 0.00221, 0*pi/180, 0*pi/180, 0, deg2rad(270)+0.00001];
+SC_state0 = kepel_statvec (SC_Kepler);
+TC_state0 = kepel_statvec (TC_Kepler);
 
-w_orbit = kepler6_to_orbit_rate(SC_Kepler);%orbit rate : [rad/s]
+SC_pos0 = [SC_state0(1),SC_state0(2),SC_state0(3)]';%ECI 
+SC_vel0 = [SC_state0(4),SC_state0(5),SC_state0(6)]';%ECI 
+TC_pos0 = [TC_state0(1),TC_state0(2),TC_state0(3)]';%ECI 
+TC_vel0 = [TC_state0(4),TC_state0(5),TC_state0(6)]';%ECI 
+
+REL_TS_POS_I_0 = TC_pos0 - SC_pos0;
+REL_TS_VEL_I_0 = TC_vel0 - SC_vel0;
+
+% w_orbit = kepler6_to_orbit_rate(SC_Kepler);%orbit rate : [rad/s]
 
 SC_Ib = [1257.52 -0.07285  -0.0345; % [kg m^2]
       -0.0728   11126.1   -24.1635;
       -0.0345  -24.1635   11354.6];
 m_tot = 3019.19 ;%[kg]
 wb0 = [0 0 0]';
-qb0 = angle2quat(deg2rad(0),deg2rad(0),deg2rad(0),'ZYX');
+qb0 = angle2quat(deg2rad(0),deg2rad(0),deg2rad(0),'ZYX')';
 vI0 = [0 0 0]';
 pI0 = [0 0 0]';
+
+TC_Ib = diag([1 1 1]);
+TC_wb0 = deg2rad([0 0 0]');
+TC_qb0 = angle2quat(deg2rad(0),deg2rad(0),deg2rad(0),'ZYX')';
 
 %% Reaction Wheel Configuration
 % Fly_Wheel_Mass = 12;%[kg]
@@ -140,41 +154,163 @@ RCS_THR_vec = [RCS1_THR , RCS2_THR , RCS3_THR , RCS4_THR, ...
                RCS9_THR , RCS10_THR, RCS11_THR, RCS12_THR, ...
                RCS13_THR, RCS14_THR, RCS15_THR, RCS16_THR];
 
-%% function 
-function out = kepler6_to_orbit_rate(kep)
-%KEPLER6_TO_ORBIT_RATE  Kepler 6요소 -> orbit rate(mean motion) 반환
+%% function
+function statvec  = kepel_statvec(kepel)
+%  [statvec] = kepel_statvec (kepel)
+%	The function kepel_statvec transforms the keplerian
+%	elements kepel into the corresponding state vector in
+%	the same reference system.
 %
-% kep = [a; e; i; RAAN; argp; M]  (rad)
-%   a [m] 만으로 mean motion n이 결정.
-% (1) - semimajor axis of the orbit in meters. 
-% (2) - eccentricity. 
-% (3) - inclinatsion in radians. 
-% (4) - right ascension of ascending node in radians. 
-% (5) - argument of perigee in radians. 
-% (6) - mean anomaly in radians.
+% Input:
+%	kepel
+% 		vector containing the keplerian elements:
+%		(1) - semimajor axis of the orbit in meters.
+%		(2) - eccentricity.
+%		(3) - inclinatsion in radians.
+%		(4) - right ascension of ascending node in radians.
+%		(5) - argument of perigee in radians.
+%		(6) - mean anomaly in radians.
+% Output:
+%	statvec
+%  		state vector in meters and meters/second.
 %
-% out:
-%   out.n_rad_s   : mean motion [rad/s]
-%   out.n_deg_s   : mean motion [deg/s]
-%   out.n_rev_day : mean motion [rev/day]
-%   out.period_s  : orbital period [s]
-%   out.period_min: orbital period [min]
+% Authors:
+%	Helio K. Kuga;
+%	Valder M. Medeiros;
+%	Valdemir Carrara		february/80		version 1.0
+%	Valdemir Carrara		july 2005		(C version)
+%   Valdemir Carrara            March/09        Matlab
 
-mu = 3.986004418e14; % Earth [m^3/s^2]
+EARTH_GRAVITY	= 3.9860064e+14;		% Earth's gravitational constant (m**3/s**2)
 
-a = kep(1);
+a    = kepel(1);	% semi-major axis
+exc  = kepel(2);	% eccentricity
 
-% mean motion
-n = sqrt(mu / (a^3));           % [rad/s]
+c1   = sqrt(1. - exc*exc);
 
-% out.n_rad_s   = n;
-% out.n_deg_s   = n * (180/pi);
-% out.n_rev_day = n * (86400/(2*pi));  % rev/day
-% 
-% out.period_s   = 2*pi / n;
-% out.period_min = out.period_s / 60;
+%     rotation matrix
 
-out = n;
+orb2iner = rotmaz(-kepel(4))*rotmax(-kepel(3))*rotmaz(-kepel(5));
+
+%     Kepler's equation
+
+E    = kepler(kepel(6), exc);
+
+sE   = sin(E);
+cE   = cos(E);
+c3   = sqrt(EARTH_GRAVITY/a)/(1. - exc*cE);
+
+%     state vector
+
+statvec = [[a*(cE - exc), a*c1*sE, 0]*orb2iner', [-c3*sE, c1*c3*cE, 0]*orb2iner'];
 
 end
 
+function [eccentric] = kepler (mean_anomaly, eccentricity)
+% [eccentric] = kepler (mean_anomaly, eccentricity)
+%	The subroutine kepler finds a solution to the
+%	kepler's equation.
+%
+% Input:
+%	mean_anomaly
+%		mean anomaly in radians.
+%	eccentricity
+%		eccentricity.
+%
+% Output:
+%	eccentric
+%		eccentric anomaly in radians.
+%
+% Authors:
+%	Valder M. de Medeiros		june/81		version 2.0
+%	Valdemir Carrara			july 2005	(C version)
+%   Valdemir Carrara            March/09        Matlab
+
+% http://www.jgiesen.de/kepler/kepler.html
+exc2    = eccentricity*eccentricity;
+am1     = mod (mean_anomaly, 2*pi);
+am2     = am1 + am1;
+am3     = am1 + am2;
+shoot   = am1 + eccentricity*(1. - 0.125*exc2)*sin(am1) + ...
+		0.5*exc2*(sin(am2) + 0.75*eccentricity*sin(am3));
+shoot   = mod (shoot, 2*pi);
+
+e1      = 1.0;
+ic      = 0;
+
+while ((abs(e1) > 1.e-12) && (ic <= 10)) 
+	e1    = (shoot - am1 - eccentricity*sin(shoot)) / ...
+			(1.0 - eccentricity*cos(shoot));
+    shoot = shoot - e1;
+    ic    = ic + 1;
+end
+
+if (ic >= 10)
+    disp ('warning ** subroutine kepler did not converge in 10 iterations');
+end
+
+eccentric = shoot;
+end
+
+function [rot_mat] = rotmax (angle)
+% [rot_mat] = rotmax (angle)
+%   To obtain the rotation matrix around the X axis given the
+%   rotation angle.
+% inputs:
+%   angle
+%       rotation angle, in radians.
+% outputs:
+%   rot_mat
+%       rotation matrix (3, 3)
+%
+
+% Valdemir Carrara, Sep, 1998
+
+coan    =  cos(angle);
+sian    =  sin(angle);
+
+rot_mat =  [1, 0, 0; 0, coan, sian; 0, -sian, coan];
+
+end
+
+function [rot_mat] = rotmay (angle)
+% [rot_mat] = rotmay (angle)
+%   To obtain the rotation matrix around the Y axis given the
+%   rotation angle.
+% inputs:
+%   angle
+%       rotation angle, in radians.
+% outputs:
+%   rot_mat
+%       rotation matrix (3, 3)
+%
+
+% Valdemir Carrara, Sep, 1998
+
+
+coan    =  cos(angle);
+sian    =  sin(angle);
+
+rot_mat =  [coan, 0, -sian; 0, 1, 0; sian, 0, coan];
+
+end
+
+function [rot_mat] = rotmaz (angle)
+% [rot_mat] = rotmaz (angle)
+%   To obtain the rotation matrix around the Z axis given the
+%   rotation angle.
+% inputs:
+%   angle
+%       rotation angle, in radians.
+% outputs:
+%   rot_mat
+%       rotation matrix (3, 3)
+%
+
+% Valdemir Carrara, Sep, 1998
+
+coan    =  cos(angle);
+sian    =  sin(angle);
+
+rot_mat =  [coan, sian, 0; -sian, coan, 0; 0, 0, 1];
+end
